ccad60a5055e22756a3f4e8f9bb9a3591c2ae086
muhammadanas123 <anasfarooqi53@gmail.com>
2022-11-17 20:12:37 +0500

    add various functionalities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(username: "test2", password: "password")
+# User.create(username: "test3", password: "password")
+# User.create(username: "test4", password: "password")
+# User.create(username: "test5", password: "password")
+# User.create(username: "test6", password: "password")
+
+
+Message.create(body: "Hi I'm a new message", user: User.find(6))
+Message.create(body: "Hi I'm a new message", user: User.find(5))
+Message.create(body: "Hi I'm a new message", user: User.find(4))
+Message.create(body: "Hi I'm a new message", user: User.find(3))
+Message.create(body: "Hi I'm a new message", user: User.find(2))
+Message.create(body: "Hi I'm a new message", user: User.find(1))
